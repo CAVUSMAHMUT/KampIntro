ea1b433a1186d51bb2bccdac18cc3a90d61bd3f1
Mahmut ÇAVUŞ <somber08@hotmail.com>
2021-05-22 17:49:29 +0300

    22.05.21

---
+++ OOP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager tasitKredisiManager = new TasitKredisiManager();
+            IKrediManager ihtiyacKredisiManager = new IhtiyacKredisiManager();
+            IKrediManager konutKredisiManager = new KonutKredisiManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(),new SmsLoggerService());
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { tasitKredisiManager, ihtiyacKredisiManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
---
+++ OOP3/TasitKredisiManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class TasitKredisiManager : IKrediManager
+    {
+        public void BiseyYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+            Console.WriteLine("Taşıt Kredisi Ödeme Planı Hesapladı");
+        }
+    }
+}
